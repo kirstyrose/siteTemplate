5e717e91ddafe58a3729fe3726943c68aea3f86d
Kirsty Rose <krhome21@gmail.com>
2015-01-27 19:08:51 +0000

    Update Scripts

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,108 +1,128 @@
-MAIN = (function ($) {
+$(function(){
 
-	init = function () {
-		// Code here runs straight away
+	function runScripts(){
 
-		var lat,
-			lng;
-		$(DOMready);
+		var setup = {
 
-		var mapObject = document.getElementById("google-map");
+			init: function(){
 
-		if (mapObject) {
+				for(var funcs in readyFunctions) {
 
-			function loadScript() {
-			  var script = document.createElement('script');
-			  script.type = 'text/javascript';
-			  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +'callback=maps';
-			  document.body.appendChild(script);
+					try {
+
+						readyFunctions[funcs]();
+
+					} catch(error) {
+
+						console.group('[Hey noob, you broke '+funcs+'... look below] - ' + error['message']);
+						console.info(error['stack'].split('\n')[1]);
+						console.groupEnd();
+
+					}
+
+				}
+
+				$(window).load(function(){
+
+					for(var funcs in loadFunctions) {
+
+						try {
+
+							loadFunctions[funcs]();
+
+						} catch(error) {
+
+							console.group('[Hey noob, you broke '+funcs+'... look below]');
+							console.info(error['stack'].split('\n')[1]);
+							console.groupEnd();
+
+						}
+
+					}
+
+				});
+
+				$(window).resize(function(){
+
+					for(var funcs in resizeFunctions) {
+
+						try {
+
+							resizeFunctions[funcs]();
+
+						} catch(error) {
+
+							console.group('[Hey noob, you broke '+funcs+'... look below]');
+							console.info(error['stack'].split('\n')[1]);
+							console.groupEnd();
+
+						}
+
+					}
+
+				});
+
+				$(window).scroll(function(){
+
+					for(var funcs in scrollFunctions) {
+
+						try {
+
+							scrollFunctions[funcs]();
+
+						} catch(error) {
+
+							console.group('[Hey noob, you broke '+funcs+'... look below]');
+							console.info(error['stack'].split('\n')[1]);
+							console.groupEnd();
+
+						}
+
+					}
+
+				});
+
 			}
-
-			window.onload = loadScript;
 
 		}
 
-	},
-
-	DOMready = function () {
-
-		// Code here runs when the DOM is ready
-		imager();
-		forms();
-
-	};
-
-	imager = function() {
-		new Imager('.js-load-img', {
-			//availableWidths: [360, 480, 600, 800, 1024],
-			lazyload: true
-		});
-	};
-
-	forms = function() {
-
-		// Form Wrapping, change any to false that you don't want.
-
-		window.slick = new slickForms({
-
-			select : true,
-			checkbox : true,
-			radio : true,
-			file: true
-
-		});
-
-		// Example form settings updates
-
-		// $('#random-form-selector').propForm('updateSettings', {
-
-		// 	wrapper : '.field-wrap',
-		// 	tooltip : true,
-		// 	compare : true,
-		// 	errorClass : 'error',
-		// 	ajax : true
-		// 	pending : null,
-		// 	success : null
-
-		// });
+		setup.init();
 
 	}
 
-	maps = function() {
+	window.loadFunctions = {
 
-		var lat = $('#google-map').data('lat');
-		var lng = $('#google-map').data('lng');
-
-		var latlng = new google.maps.LatLng(lat, lng);
-		var myOptions = {
-			zoom: 14,
-			center: latlng,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		var map = new google.maps.Map(document.getElementById("google-map"),
-				myOptions);
-
-		var image = new google.maps.MarkerImage('/assets/img/map-icon.png',
-			// This marker is 20 pixels wide by 32 pixels tall.
-			new google.maps.Size(40, 57),
-			// The origin for this image is 0,0.
-			new google.maps.Point(0, 0),
-			// The anchor for this image is the base of the flagpole at 0,32.
-			new google.maps.Point(20, 56));
-
-		var marker = new google.maps.Marker({
-			position: latlng,
-			map: map,
-			icon: image
-		});
+		method1: function(){
+			console.log('loadFunctions ready');
+		}
 
 	}
 
-	return {
-		start : init
-	};
+	window.scrollFunctions = {
+		
+		method2: function(){
+			console.log('scrollFunctions ready');
+		}
 
+	}
 
-})(jQuery);
+	window.resizeFunctions = {
+		
+		method3: function(){
+			console.log('resizeFunctions ready');
+		}
 
-MAIN.start();
+    }
+	
+	window.readyFunctions  = {
+
+		method4: function(){
+			console.log('readyFunctions ready');
+		}
+
+	}
+
+	runScripts();
+
+});
+
